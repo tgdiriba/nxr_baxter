e00529a37757b69f8f2de0e5bd46faab3f3bb257
Jon <jonathanrovira2016@u.northwestern.edu>
2014-06-23 14:58:26 -0500

    continue development on ar_block

--- include/ar_block.h
+++ include/ar_block.h
@@ -1,9 +1,21 @@
 #include <geometry_msgs/Pose.h>
+#include <moveit_msgs/CollisionObject>
 
 class ar_block {
 public:
+	ar_block();
+	ar_block(float *dims, int id = 0);	
+	moveit_msgs::CollisionObject toCollisionObject(std::string planning_frame = std::string("/base"));	
+
+	geometry_msgs::Pose pose;
+	int id;
 	
-	union dimensions {
-		float 
+	union {
+		float all[3];
+		struct {
+			float x;
+			float y;
+			float z;
+		};
 	};
 };
